8202ef4d4fa9a7ab73a5f6815c57e5489999e0b1
Rhythm1821 <106919276+Rhythm1821@users.noreply.github.com>
2022-11-08 11:48:09 +0530

    Add files via upload

---
+++ assign_1.py
@@ -0,0 +1,17 @@
+# Ans-6
+bacon = 22
+bacon + 1
+print(bacon)
+
+#Ans-7
+print("spam" + "spamspam")
+print("spam"*3)
+
+#Ans-8
+#Because 100 is a data type and by convention data types cannot be set as variables
+
+#Ans-9
+
+
+#Ans-10
+print("I have eaten " + "99" + " burritos.")

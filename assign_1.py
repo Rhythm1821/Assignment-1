c3f691a77f7df092e5edf9b3c82a108c25d7b218
Rhythm1821 <106919276+Rhythm1821@users.noreply.github.com>
2022-11-08 11:49:33 +0530

    Update assign_1.py

--- assign_1.py
+++ assign_1.py
@@ -1,3 +1,36 @@
+# Ans-1  
+#   *       - Expression
+#   "hello" - Value(String)
+#   -87.8   - Value(Float)
+#   -       - Expression
+#   /       - Expression
+#   +       - Expression
+#   6       - Value(Int)
+
+# Ans-2 
+#    String   - Strings are a part of data types which are used to define the values of a variable.
+#    Variable - Variables are used to assign values to certain elements so that they can be further used to write code efficiently and can be understood in an easy manner.
+
+# Ans-3
+#   Numeric data type - Used to store values in numeric form.
+#     contains three types:
+#         int     - Contains numeric values without decimal 
+#         float   - Contains numeric values with dacimals
+#         complex - Contains numeric values with complex numbers
+
+#   Boolean data type - Contains values with true or false
+
+#   Dictionary - Dictionaries are used to store data values in key:value pairs.
+#               Example-: a = {"name": "Sam" , "age": 12}
+
+# Ans-4 An expression is a combination of operators and operands that is interpreted to produce some other value
+
+
+# Ans-5 A statement is used for creating and declaring variables while an expression is used for evaluating results
+#   Example-: x = 10      (A statement)
+#             x = x + 10  (An expression)
+
+
 # Ans-6
 bacon = 22
 bacon + 1
